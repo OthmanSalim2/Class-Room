1127c2f476a4ed5df977263447e65e247752c284
othman salim <othmansalim423@gmail.com>
2025-01-23 18:54:40 +0200

    Preparing DB tables and models

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+//    return view('welcome');
+    $f = new IntlDateFormatter(
+        'ar@calendar=islamic',
+        IntlDateFormatter::FULL,
+        IntlDateFormatter::FULL,
+        'Asia/Gaza',
+        IntlDateFormatter::TRADITIONAL,
+    );
+
+    echo $f->format(new DateTime());
 });
